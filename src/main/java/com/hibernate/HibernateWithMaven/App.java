f1ec65aeb432d67a28f7c572b736eec72798ab31
ajju5941 <aijazahmed.93@gmail.com>
2021-10-01 15:10:05 +0530

    Maven Project created for Hibernate

---
+++ src/main/java/com/hibernate/HibernateWithMaven/App.java
@@ -0,0 +1,13 @@
+package com.hibernate.HibernateWithMaven;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
