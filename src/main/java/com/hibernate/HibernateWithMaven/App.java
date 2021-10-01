a26fb8251b9056ccdc67c1fd8336a0141fef9d0d
ajju5941 <aijazahmed.93@gmail.com>
2021-10-01 16:32:26 +0530

    Created Student class and mapped as Entity using anotation

--- src/main/java/com/hibernate/HibernateWithMaven/App.java
+++ src/main/java/com/hibernate/HibernateWithMaven/App.java
@@ -1,8 +1,11 @@
 package com.hibernate.HibernateWithMaven;
 
 
+import org.hibernate.Session;
 import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
 import org.hibernate.cfg.Configuration;
+
 
 /**
  * Hello world!
@@ -20,8 +23,19 @@ public class App
         cfg.configure("hibernate.cfg.xml");
         SessionFactory factory = cfg.buildSessionFactory();
         
-        System.out.println(factory);
+        //Creating Student
+        Student st = new Student();
+        st.setId(1);
+        st.setName("Aijaz");
+        st.setCity("Mumbai");
+        System.out.println(st);
         
+        Session session = factory.openSession();	
+        
+        Transaction tx = session.beginTransaction();
+        session.save(st);
+        tx.commit();
+        session.close();
         
     }
 }
