5332574eefa3d4bef640e82c3bc4af32a8a9e0a1
ajju5941 <aijazahmed.93@gmail.com>
2021-10-01 15:42:02 +0530

    addede depencicies in pom.xml and created hibernate.cfg.xml

--- src/main/java/com/hibernate/HibernateWithMaven/App.java
+++ src/main/java/com/hibernate/HibernateWithMaven/App.java
@@ -1,13 +1,27 @@
 package com.hibernate.HibernateWithMaven;
 
+
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
 /**
  * Hello world!
  *
  */
 public class App 
 {
+    /**
+     * @param args
+     */
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        System.out.println( "Project Started" );
+        Configuration cfg = new Configuration();
+        cfg.configure("hibernate.cfg.xml");
+        SessionFactory factory = cfg.buildSessionFactory();
+        
+        System.out.println(factory);
+        
+        
     }
 }
