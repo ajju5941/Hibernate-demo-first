e0310b388d95a507cd2ab30e6f25483c7cff35d7
ajju5941 <aijazahmed.93@gmail.com>
2021-10-16 14:27:51 +0530

    HQL Example

---
+++ src/main/java/com/hibernate/hql/HQLExample.java
@@ -0,0 +1,32 @@
+package com.hibernate.hql;
+
+import java.util.List;
+
+import org.hibernate.*;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+import com.hibernate.HibernateWithMaven.Student;
+
+public class HQLExample {
+	
+	public static void main(String[] args) {
+	
+		Configuration cfg = new Configuration().configure("hibernate.cfg.xml");
+		SessionFactory factory = cfg.buildSessionFactory();
+		
+		Session s = factory.openSession();
+		
+		String query = "from Student";
+		Query q = s.createQuery(query);
+
+		List<Student> list = q.list();
+		
+		for(Student student:list)
+		{
+			System.out.println(student.getName()+" : "+student.getCerti().getCourse());
+		}
+	}
+	
+}
